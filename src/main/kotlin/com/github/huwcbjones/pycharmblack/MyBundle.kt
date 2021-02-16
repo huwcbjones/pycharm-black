8646fd2250468d26711791adee79e07c1e550384
GitHub Action <action@github.com>
2021-02-16 18:05:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/huwcbjones/pycharmblack/MyBundle.kt
+++ src/main/kotlin/com/github/huwcbjones/pycharmblack/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.huwcbjones.pycharmblack
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/huwcbjones/pycharmblack/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/huwcbjones/pycharmblack/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.huwcbjones.pycharmblack.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.huwcbjones.pycharmblack.MyBundle
 
 class MyApplicationService {
 
